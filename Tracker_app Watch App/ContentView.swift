572fa37f7b32a0c61f066243e6fa3fd289c8cecd
FOYSAL <30071088@students.southwales.ac.uk>
2026-02-22 23:35:20 +0000

    Initial Commit

---
+++ Tracker_app Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tracker_app Watch App
+//
+//  Created by Hossain S M F (FCES) on 22/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tracker_app Watch App/Tracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_appApp.swift
+//  Tracker_app Watch App
+//
+//  Created by Hossain S M F (FCES) on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tracker_app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
